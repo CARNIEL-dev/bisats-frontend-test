421d5c513a73d11760e3057aa2eae18475eb1071
Alade Faruk <alade.faruk11@gmail.com>
2025-05-29 14:31:17 +0100

    feat: notifications update #123

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -126,7 +126,13 @@ const Header: React.FC<HeaderProps> = ({ currentPage }) => {
                         
                         <div className='mx-3 flex items-center border-b-[1px] py-2 border-[#D6DAE1]'>
                             <div className='flex items-center'>
-                                <h1 className='text-[#0A0E12] text-[18px] font-[600] leading-[32px]'>Notifications</h1>
+                                <h1 className='text-[#0A0E12] text-[18px] flex items-center font-[600] leading-[32px]'>Notifications
+                                    <svg className="mx-1.5" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8 4.29297V6.51297" stroke="#17A34A" stroke-miterlimit="10" stroke-linecap="round" />
+                                    <path d="M8.01522 1.33301C5.56189 1.33301 3.57522 3.31967 3.57522 5.77301V7.17301C3.57522 7.62634 3.38855 8.30634 3.15522 8.69301L2.30855 10.1063C1.78855 10.9797 2.14855 11.953 3.10855 12.273C6.29522 13.333 9.74188 13.333 12.9286 12.273C13.8286 11.973 14.2152 10.9197 13.7286 10.1063L12.8819 8.69301C12.6486 8.30634 12.4619 7.61967 12.4619 7.17301V5.77301C12.4552 3.33301 10.4552 1.33301 8.01522 1.33301Z" stroke="#17A34A" stroke-miterlimit="10" stroke-linecap="round" />
+                                    <path d="M10.2213 12.5469C10.2213 13.7669 9.22125 14.7669 8.00125 14.7669C7.39458 14.7669 6.83458 14.5135 6.43458 14.1135C6.03458 13.7135 5.78125 13.1535 5.78125 12.5469" stroke="#17A34A" stroke-miterlimit="10" />
+                                </svg>
+                                </h1>
 
                             </div>
 
